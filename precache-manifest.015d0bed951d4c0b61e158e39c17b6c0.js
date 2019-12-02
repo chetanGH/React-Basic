b8c5f7fa19bbe629efb413bdd7ded1b36889e1c5
Chetan Hebsur <chetanhebsur4@gmail.com>
2019-12-02 23:06:44 +0530

    Updates

---
+++ precache-manifest.015d0bed951d4c0b61e158e39c17b6c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bab9a8e272c810284a678f59a2a35f0",
+    "url": "/React-Basic/index.html"
+  },
+  {
+    "revision": "19cdc4bbcf69809c8022",
+    "url": "/React-Basic/static/css/main.a980791e.chunk.css"
+  },
+  {
+    "revision": "dd500705f34bedbd0ab9",
+    "url": "/React-Basic/static/js/2.8c9121d6.chunk.js"
+  },
+  {
+    "revision": "19cdc4bbcf69809c8022",
+    "url": "/React-Basic/static/js/main.12f39e81.chunk.js"
+  },
+  {
+    "revision": "f6bf0ef8a0303c670c7e",
+    "url": "/React-Basic/static/js/runtime-main.13228583.js"
+  }
+]);
